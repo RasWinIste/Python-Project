7aa72f636099ab4ee2b3a15d202c6cc565afd208
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-16 17:21:30 +0200

    Update user.py

--- pythonProject/models/user.py
+++ pythonProject/models/user.py
@@ -1,7 +1,7 @@
 class User:
     def __init__(self, name: str):
         self.name = name
-        # Comment in the code
+        # Comment in the code v2
 
     def __repr__(self):
         return f"<User name={self.name}>"
